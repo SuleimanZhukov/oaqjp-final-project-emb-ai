a01927a8dc217bb1f36da8e0f14a5fb39f56e404
Suleiman Zhukov <suleimanzhukov@gmail.com>
2025-07-28 14:24:25 -0400

    pylind

--- server.py
+++ server.py
@@ -3,26 +3,34 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
+# Index html page
 @app.route("/")
 def get_index_template():
     return render_template("index.html")
 
+# Analysing emotion route
 @app.route("/emotionDetector")
-def detect_emotion():
+def sent_emotion():
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
 
     if response['dominant_emotion'] == None:
         return "Invalid text! Please try again!"
 
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}".format(
-            response['anger'],
-            response['disgust'],
-            response['fear'],
-            response['joy'],
-            response['sadness'],
-            response['dominant_emotion'],
-        )
+    anger = response["anger"]
+    disgust = response["disgust"]
+    fear = response["fear"]
+    joy = response["joy"]
+    sadness = response["sadness"]
+    dominant = response["dominant_emotion"]
+
+    message = (
+        f"For the given statement, the system response is 'anger': {anger}, "
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+        f"The dominant emotion is {dominant}."
+    )
+
+    return message
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
